7cf9f71e0743745b90fe543d8fa8e289c22c1859
Noemi Mendoza <116498194+Mendozanoe@users.noreply.github.com>
2024-08-03 21:06:27 -0300

    initial

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  base:"/TastyMealsBB/",
   plugins: [react()],
-  server: {
-    port: 3000 // Cambia el puerto a 3000 o al que prefieras
-  }
+
 })
