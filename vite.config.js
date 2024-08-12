5b28737a3eb47fd40490bd2a10b50f409d2db29b
Noemi Mendoza <116498194+Mendozanoe@users.noreply.github.com>
2024-08-12 00:06:48 -0300

    optimization

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import postcss from 'postcss'
 
 export default defineConfig({
-  base:"/TastyMealsBB/",
+  base: "/TastyMealsBB/",
   plugins: [react()],
-
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+    css: {
+      postcss: {
+        plugins: [
+          postcss.plugin('postcss-cssnano', () => cssnano())
+        ]
+      }
+    }
+  },
 })
